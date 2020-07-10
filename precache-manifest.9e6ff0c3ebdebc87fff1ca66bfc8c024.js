68924daafe5f42be4ee0eccfaf6d9cf83b2a01d7
kartikeydwi1997 <kartikeydwi97@gmail.com>
2020-07-10 21:11:19 +0530

    Updates

---
+++ precache-manifest.9e6ff0c3ebdebc87fff1ca66bfc8c024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c8011fb368f5e390fa9d0cbd58b88d",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "fb2b920251f183f5e959",
+    "url": "/Covid-Tracker/static/css/main.f878b1dd.chunk.css"
+  },
+  {
+    "revision": "7eeafd0ac158eb900ee8",
+    "url": "/Covid-Tracker/static/js/2.39b5e4c1.chunk.js"
+  },
+  {
+    "revision": "fb2b920251f183f5e959",
+    "url": "/Covid-Tracker/static/js/main.092681b5.chunk.js"
+  },
+  {
+    "revision": "f394e6bc8179cf29c858",
+    "url": "/Covid-Tracker/static/js/runtime~main.0c67acba.js"
+  }
+]);
